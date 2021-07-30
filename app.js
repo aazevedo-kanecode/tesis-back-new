82b6a64d0e9907385da9678b0095f87a721a341e
angelsucasas <angel.alejandro.sucasas08@gmail.com>
2021-07-30 03:30:45 -0400

    cors fixing

--- app.js
+++ app.js
@@ -58,13 +58,13 @@ app.use(cors());
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", process.env.FRONT_END_ORIGIN);
-	res.header("Access-Control-Allow-Credentials", true);
+	//res.header("Access-Control-Allow-Credentials", true);
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
 	);
 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-	res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+	//res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
 	next();
 });
 
